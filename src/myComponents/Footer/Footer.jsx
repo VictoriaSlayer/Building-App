29bb83bc3fd34c803d07f8fc70f7b136d5230788
Ионов Святослав <s.ionov@corp.fsk-tech>
2022-06-27 17:06:08 +0300

    Problems with SVG icons

--- src/myComponents/Footer/Footer.jsx
+++ src/myComponents/Footer/Footer.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+
 import style from "./Footer.module.css";
 import FooterItem from "./FooterItem.jsx";
+
+import {ReactComponent as Organization} from './../../Images/Icons/organization.svg';
+import {ReactComponent as Worker} from './../../Images/Icons/worker.svg';
+import {ReactComponent as Photo} from './../../Images/Icons/photo.svg';
+import {ReactComponent as Message} from './../../Images/Icons/message.svg';
+import {ReactComponent as Calc} from './../../Images/Icons/calc.svg';
+import {ReactComponent as Graph} from './../../Images/Icons/graph.svg';
+
+const iconsArray = ["Organization", "Worker", "Photo", "Message", "Calc", "Graph"];
+
 function Footer(props) {
     return(
-        <footer className={style.footer}>
-            <FooterItem/>
-        </footer>
+        <BrowserRouter>
+            {/* <div className={style.footer}>
+                <FooterItem/>
+                <Graph/>
+            </div> */}
+            {iconsArray.map(item => {
+                // console.log(`${item}`);
+                 return `<Link><${item}/></Link>`
+            })}
+            <Graph/>
+        </BrowserRouter>
     )
 }
 
--- src/myComponents/Footer/FooterItem.jsx
+++ src/myComponents/Footer/FooterItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import style from "./FooterItem.module.css";
+import {ReactComponent as Svg} from './../../Images/Icons/organization.svg';
 
 function FooterItem(props) {
     return(
         <figure className={style.footer__Item}>
-            <img src="./../../../New template/Icons png/Geoposition.png"/>
+            {/* <img src="./../../../New template/Icons png/Geoposition.png"/> */}
+            <Svg className={style.svgColor}/>
             <figcaption>Объекты</figcaption>
         </figure>
     )
