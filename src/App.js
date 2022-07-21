b4608d2c8ecdd155a83d37fef3c166e3d225b192
Ионов Святослав <s.ionov@corp.fsk-tech>
2022-07-21 16:52:54 +0300

    Added Calendar CSS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import classes from './App.module.css';
 import Companies from './myComponents/Companies/Companies';
 import {Routes, Route, Link} from "react-router-dom";
 
+import MyCalendar from './myComponents/Calendar/MyCalendar';
+
 function App(props) {
   return (
     <div className={classes.main__container}>
@@ -14,6 +16,7 @@ function App(props) {
             <Route path={item.dropdownmenu.links.dropdown} element={<Companies links={item.dropdownmenu.links}/>}/>
           )}
         </Routes>
+        <MyCalendar/>
         <Footer data={props.data}/>
     </div>
   );
