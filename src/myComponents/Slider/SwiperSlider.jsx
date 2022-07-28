5027a1b7faef0c19b5fa7330e719e5a848634b48
Ионов Святослав <s.ionov@corp.fsk-tech>
2022-07-28 12:54:01 +0300

    buffer

--- src/myComponents/Slider/SwiperSlider.jsx
+++ src/myComponents/Slider/SwiperSlider.jsx
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore from 'swiper';
+import SwiperCore, {Navigation, Pagination, Thumbs} from 'swiper';
 import '../../../node_modules/swiper/swiper-bundle.css';
 import './SwiperSlider.css';
 // import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
+SwiperCore.use([Navigation, Pagination, Thumbs]);
 
 function SwiperSlider(){
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const slides = [];
+  const thumbs = [];
+
+  for(let i = 1; i < 10; i++) {
+   slides.push(
+    <SwiperSlide key={`masonry-img${i}`} tag="li"><img  src={`./Photos/${i}.jpg`} alt={`каменная кладка {i}`}/></SwiperSlide>
+    );
+    thumbs.push(
+      <SwiperSlide key={`masonryThumbs-img${i}`} tag="li"><img  src={`./Photos/${i}.jpg`} alt={`каменная кладка {i}`}/></SwiperSlide>
+    )
+  }
+
+
+
   return (
     <>
-    <Swiper id="main"
-    // install Swiper modules
-    // modules={[Navigation, Pagination, Scrollbar, A11y]}
-    spaceBetween={50}
+    <Swiper tag="section" wrapperTag="ul" id="main"
+    thumbs = {{swiper: thumbsSwiper}}
+    spaceBetween={0}
     slidesPerView={3}
     navigation
-    pagination={{ clickable: true }}
-    scrollbar={{ draggable: true }}
-    onSwiper={(swiper) => console.log(swiper)}
-    onSlideChange={() => console.log('slide change')}
+    pagination
     >
-    <SwiperSlide><img src="./Photos/1.jpg" alt="картинка" /></SwiperSlide>
-    <SwiperSlide><img src="./Photos/2.jpg" alt="картинка" /></SwiperSlide>
-    <SwiperSlide><img src="./Photos/3.jpg" alt="картинка" /></SwiperSlide>
-    <SwiperSlide><img src="./Photos/4.jpg" alt="картинка" /></SwiperSlide>
-    <SwiperSlide><img src="./Photos/5.jpg" alt="картинка" /></SwiperSlide>
-    <SwiperSlide><img src="./Photos/6.jpg" alt="картинка" /></SwiperSlide>
-    <SwiperSlide><img src="./Photos/7.jpg" alt="картинка" /></SwiperSlide>
-    <SwiperSlide><img src="./Photos/8.jpg" alt="картинка" /></SwiperSlide>
-    <SwiperSlide><img src="./Photos/9.jpg" alt="картинка" /></SwiperSlide>
+      {slides}
+  </Swiper>
+  <Swiper
+  id="thumbs"
+  spaceBetween={0}
+  slidesPerView={4}
+  onSwiper={setThumbsSwiper}
 
+  >
+    {thumbs}
   </Swiper>
     </>
   );
