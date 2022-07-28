eb6e946555a1839bd13ab8b9bb5b2543f2f5787c
Ионов Святослав <s.ionov@corp.fsk-tech>
2022-07-28 17:18:48 +0300

    buffer

--- src/myComponents/Slider/SwiperSlider.jsx
+++ src/myComponents/Slider/SwiperSlider.jsx
@@ -1,24 +1,21 @@
 import React, {useState} from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, {Navigation, Pagination, Thumbs} from 'swiper';
+import SwiperCore, {Navigation, Pagination} from 'swiper';
 import '../../../node_modules/swiper/swiper-bundle.css';
 import './SwiperSlider.css';
 // import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
-SwiperCore.use([Navigation, Pagination, Thumbs]);
+SwiperCore.use([Navigation, Pagination]);
 
 function SwiperSlider(){
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
   const slides = [];
-  const thumbs = [];
+
 
   for(let i = 1; i < 10; i++) {
    slides.push(
     <SwiperSlide key={`masonry-img${i}`} tag="li"><img  src={`./Photos/${i}.jpg`} alt={`каменная кладка {i}`}/></SwiperSlide>
     );
-    thumbs.push(
-      <SwiperSlide key={`masonryThumbs-img${i}`} tag="li"><img  src={`./Photos/${i}.jpg`} alt={`каменная кладка {i}`}/></SwiperSlide>
-    )
   }
 
 
@@ -26,25 +23,59 @@ function SwiperSlider(){
   return (
     <>
     <Swiper tag="section" wrapperTag="ul" id="main"
-    thumbs = {{swiper: thumbsSwiper}}
     spaceBetween={0}
     slidesPerView={3}
     navigation
     pagination
+    onSlideToClickedSlide={true}
     >
       {slides}
-  </Swiper>
-  <Swiper
-  id="thumbs"
-  spaceBetween={0}
-  slidesPerView={4}
-  onSwiper={setThumbsSwiper}
-
-  >
-    {thumbs}
   </Swiper>
     </>
   );
 }
 
 export default SwiperSlider;
+
+
+// SwiperCore.use([Navigation, Pagination, Thumbs]);
+
+// function SwiperSlider(){
+//   const [thumbsSwiper, setThumbsSwiper] = useState(null);
+//   const slides = [];
+//   const thumbs = [];
+
+//   for(let i = 1; i < 10; i++) {
+//    slides.push(
+//     <SwiperSlide key={`masonry-img${i}`} tag="li"><img  src={`./Photos/${i}.jpg`} alt={`каменная кладка {i}`}/></SwiperSlide>
+//     );
+//     thumbs.push(
+//       <SwiperSlide key={`masonryThumbs-img${i}`} tag="li"><img  src={`./Photos/${i}.jpg`} alt={`каменная кладка {i}`}/></SwiperSlide>
+//     )
+//   }
+
+
+
+//   return (
+//     <>
+//     <Swiper tag="section" wrapperTag="ul" id="main"
+//     thumbs = {{swiper: thumbsSwiper}}
+//     spaceBetween={0}
+//     slidesPerView={3}
+//     navigation
+//     pagination
+//     >
+//       {slides}
+//   </Swiper>
+//   <Swiper
+//   id="thumbs"
+//   spaceBetween={0}
+//   slidesPerView={4}
+//   onSwiper={setThumbsSwiper}
+
+//   >
+//     {thumbs}
+//   </Swiper>
+//     </>
+//   );
+// }
