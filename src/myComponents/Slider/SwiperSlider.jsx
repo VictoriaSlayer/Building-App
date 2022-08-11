cbcea3c58fcd7a062b1aa2e3546e51a840656a8e
Ионов Святослав <s.ionov@corp.fsk-tech>
2022-08-11 11:04:26 +0300

    almost complete slider

--- src/myComponents/Slider/SwiperSlider.jsx
+++ src/myComponents/Slider/SwiperSlider.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, {Navigation, Pagination} from 'swiper';
 import '../../../node_modules/swiper/swiper-bundle.css';
@@ -12,12 +12,16 @@ SwiperCore.use([Navigation, Pagination]);
 
 function SwiperSlider(){
   let {id} = useParams();
+  let navigate = useNavigate();
+  let closeButton = () => navigate(-1);
 
   const slides = [];
 
   for(let i = 0; i < 10; i++) {
    slides.push(
-    <SwiperSlide key={`masonry-img${i}`} tag="li" containerClass="swiper-zoom-container" zoom="true" toggle="true"><img src={`/Photos/${i}.jpg`} alt={`фото ${i}`}/><button className="closeButton">{'\u00d7'}</button></SwiperSlide>
+    <SwiperSlide key={`masonry-img${i}`} tag="li" containerClass="swiper-zoom-container" zoom="true" toggle="true"><img src={`/Photos/${i}.jpg`} alt={`фото ${i}`}/>
+      <button className="closeButton" onClick = {closeButton}>{'\u00d7'}</button>
+    </SwiperSlide>
     );
   }
 
