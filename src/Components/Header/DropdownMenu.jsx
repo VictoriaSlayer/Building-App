4ac3e92c218d998fc1261071113829ac4407821f
Alex <iseldomused@gmail.com>
2022-08-26 19:44:11 +0300

    refactoring

--- src/Components/Header/DropdownMenu.jsx
+++ src/Components/Header/DropdownMenu.jsx
@@ -12,31 +12,22 @@ class DropdownMenu extends React.Component {
     }
 
     clickedItem = (id) => {
-        if (this.state.opened === true) {
-            this.setState(state => ({
-                chosenId: id,
-            }));
-        }
         this.setState(state => ({
+            chosenId: id,
             opened: !state.opened
         }));
     }
 
     render() {
-        // преобразовываем данные с пропсов в массив и маппим их после
-        const items = this.props.data;
-        const dropdownObjects = [];
-
-        dropdownObjects.push(items[this.state.chosenId]);
+        const rawItems = this.props.data;
+        const processedItems = [rawItems[this.state.chosenId]];
         if (this.state.opened) {
-            for (let i in items) {
-                dropdownObjects.push(items[i]);
-            }
+            processedItems.push(...rawItems);
         }
 
         return (
             <div className={style.DropdownMenu}>
-                {dropdownObjects.map((item, index) => (
+                {processedItems.map((item, index) => (
                     <DropdownMenuItem
                         name={item.name}
                         address={item.address}
